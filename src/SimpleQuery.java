f36fb34da631d3ee8038dce7b122137e7c499d6a
Haner Avella <havella@cysce.com>
2024-06-02 18:42:38 -0500

    Acomoda el query y la impresión para la un query sencillo con tipos de datos basicos

--- src/SimpleQuery.java
+++ src/SimpleQuery.java
@@ -1,6 +1,7 @@
 import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Struct;
@@ -11,26 +12,36 @@ import oracle.jdbc.*;
 public class SimpleQuery {
 	public static void printEmployees(Connection connection)
 			throws SQLException {
+
 		// Statement and ResultSet are AutoCloseable and closed automatically.
+		String sqlQuery = "select employee_id, concat(concat(first_name, ' '), last_name) as full_name from testuser.employees where employee_id>6";
 		try (Statement statement = connection.createStatement()) {
 			try (ResultSet resultSet = statement
-					.executeQuery("select * from testuser.employees where employee_id>6")) {
-				System.out.println("FIRST_NAME" + "  " + "LAST_NAME");
-				System.out.println("---------------------");
+					.executeQuery(sqlQuery)) {
 
-// 				ResultSet rs= stmt.executeQuery("SELECT * FROM struct_table");
-// java.sql.Struct jdbcStruct = (java.sql.Struct)rs.getObject(1);
+				// Acá trae la metaData del Result Set
+				ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+				int columnCount = resultSetMetaData.getColumnCount();
 
+				// El ciclo while recorre el Result Set
 				while (resultSet.next()) {
-					System.out.println("---------------------"+resultSet.getFetchSize());
-					BigDecimal employeeId = (BigDecimal) resultSet.getObject(1);
-					Struct person = (Struct) resultSet.getObject(8);
-					Object [] array = person.getAttributes();
-					StringBuffer sbuf = new StringBuffer();
-					sbuf.append(employeeId.toString() + " (),");
-					for (Object element : array) {
-						sbuf.append(element.toString() + " (),");
+					for (int i = 1; i <= columnCount; i++) {
+						if (i > 1)
+							System.out.print(",  ");
+						String columnValue = resultSet.getString(i);
+						System.out.print(columnValue + " " + resultSetMetaData.getColumnName(i));
 					}
+					System.out.println("");
+
+					// System.out.println("---------------------"+resultSet.getFetchSize());
+					// BigDecimal employeeId = (BigDecimal) resultSet.getObject(1);
+					// Struct person = (Struct) resultSet.getObject(8);
+					// Object [] array = person.getAttributes();
+					// StringBuffer sbuf = new StringBuffer();
+					// sbuf.append(employeeId.toString() + " (),");
+					// for (Object element : array) {
+					// sbuf.append(element.toString() + " (),");
+					// }
 
 					// sbuf.append(person.getSQLTypeName() + ": ");
 
@@ -42,9 +53,9 @@ public class SimpleQuery {
 					// sbuf.append(attrs[i].stringValue() + " :" + i + " ()");
 					// }
 					// Print the stringBuffer value
-					System.out.println(sbuf.toString());
+					// System.out.println(sbuf.toString());
 
-					System.out.println(resultSet.getString(1));
+					// System.out.println(resultSet.getString(1));
 					// System.out.println(resultSet.getString(1) + " "
 					// + resultSet.getString(2));
 					// + resultSet.getString(7) + " ");
