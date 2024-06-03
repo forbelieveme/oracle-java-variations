7c7f5152999bc85d2094101c688ef7028e6d4544
Haner Avella <havella@cysce.com>
2024-06-02 23:55:20 -0500

    Actualiza el main para realizar el llamado al arrayreturn

--- src/ConnectionMain.java
+++ src/ConnectionMain.java
@@ -17,33 +17,35 @@ public class ConnectionMain {
 	final static String DB_PASSWORD = "password";
 
 	public static void main(String[] args) throws SQLException {
-		
-		Properties info = new Properties();     
-	    info.put(OracleConnection.CONNECTION_PROPERTY_USER_NAME, DB_USER);
-	    info.put(OracleConnection.CONNECTION_PROPERTY_PASSWORD, DB_PASSWORD);
-	    
-	    OracleDataSource ods = new OracleDataSource();
-	    ods.setURL(DB_URL);    
-	    ods.setConnectionProperties(info);
-	    
-	    try (OracleConnection connection = (OracleConnection) ods.getConnection()) {
-	        // Get the JDBC driver name and version 
-	        DatabaseMetaData dbmd = connection.getMetaData();       
-	        System.out.println("Driver Name: " + dbmd.getDriverName());
-	        System.out.println("Driver Version: " + dbmd.getDriverVersion());
-	        System.out.println();
-	        
-			// Perform a database operation 
-	        // SimpleQuery simpleQuery = new SimpleQuery();
-	        // simpleQuery.printEmployees(connection);
-	        
-			TypeQuery typeQuery = new TypeQuery();
-	        typeQuery.printEmployees(connection);
-	      }  
+
+		Properties info = new Properties();
+		info.put(OracleConnection.CONNECTION_PROPERTY_USER_NAME, DB_USER);
+		info.put(OracleConnection.CONNECTION_PROPERTY_PASSWORD, DB_PASSWORD);
+
+		OracleDataSource ods = new OracleDataSource();
+		ods.setURL(DB_URL);
+		ods.setConnectionProperties(info);
+
+		try (OracleConnection connection = (OracleConnection) ods.getConnection()) {
+			// Get the JDBC driver name and version
+			DatabaseMetaData dbmd = connection.getMetaData();
+			System.out.println("Driver Name: " + dbmd.getDriverName());
+			System.out.println("Driver Version: " + dbmd.getDriverVersion());
+			System.out.println();
+
+			// Perform a database operation
+			// SimpleQuery simpleQuery = new SimpleQuery();
+			// simpleQuery.printEmployees(connection);
+
+			// TypeQuery typeQuery = new TypeQuery();
+			// typeQuery.printEmployees(connection);
+
+			ArrayReturnQuery arrayReturnQuery = new ArrayReturnQuery();
+			// arrayReturnQuery.printEmployees(connection);
+			arrayReturnQuery.printEmployeeById(connection, 5);
+		}
 		System.out.println("Hello World");
 
 	}
-	
-	
 
 }
